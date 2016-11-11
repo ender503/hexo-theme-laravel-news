9eb93c7d6d531acc20089a2cf1d131f0e5a3d100
Ting-Han Su <seph.serg@gmail.com>
2016-11-11 20:54:40 +0800

    Setup Laravel News Style

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass 
+var errorHandler = function (err) {
+    console.log(err.toString());
+    this.emit('end');
+  };
+
+// 一次性编译 Sass
 gulp.task('sass', function() {
     return gulp.src('./source/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        // .pipe(sass({outputStyle: 'compressed', includePaths: scssIncludePaths}))
+        .pipe(sass())
+        .on('error', errorHandler)
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
 });
 
+
+
+
 // 实时编译
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
+    gulp.watch('./source/scss/_partial/**/*.scss', ['sass']);
     gulp.watch('./source/scss/*.scss', ['sass']);
 });
